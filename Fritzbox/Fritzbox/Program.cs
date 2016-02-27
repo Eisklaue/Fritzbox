ac5b574b299e7028fc3a7a5fe2bcc73e9fd937a5
Andreas Fritz <fritz.andreas@outlook.com>
2016-02-27 11:31:40 +0100

    First Version

---
+++ Fritzbox/Fritzbox/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using Fritzbox.src;
+
+namespace Fritzbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "SmartHomeToolUser";
+            string password = "Eisklaue23#";
+
+            login log = new login(username,password);
+
+            string sid = log.GetSessionId();
+        }
+    }
+}
