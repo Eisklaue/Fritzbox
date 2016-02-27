37b5cb4e09d642a917cfc0c0be38ffe004a451b1
Andreas Fritz <fritz.andreas@outlook.com>
2016-02-27 11:29:58 +0100

    Login fertig

---
+++ Fritzbox/Fritzbox/src/login.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Security.Cryptography;
+using System.Xml.Linq;
+using System.IO;
+
+namespace Fritzbox.src
+{
+    class login
+    {
+        private string username;
+        private string password;
+
+        public login (string username, string password)
+        {
+            this.password = password;
+            this.username = username;
+        }
+        public string GetSessionId()
+        {
+            XDocument doc = XDocument.Load(@"http://fritz.box/login_sid.lua");
+            string sid = GetValue(doc, "SID");
+            if (sid == "0000000000000000")
+            {
+                string challenge = GetValue(doc, "Challenge");
+                string uri = @"http://fritz.box/login_sid.lua?username=" +
+               this.username + @"&response=" + GetResponse(challenge);
+                doc = XDocument.Load(uri);
+                sid = GetValue(doc, "SID");
+            }
+            return sid;
+        }
+        public string GetResponse(string challenge)
+        {
+            return challenge + "-" + GetMD5Hash(challenge + "-" + this.password);
+        }
+
+        public string GetMD5Hash(string input)
+        {
+            MD5 md5Hasher = MD5.Create();
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hasher.ComputeHash(Encoding.Unicode.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+        public string GetValue(XDocument doc, string name)
+        {
+            XElement info = doc.FirstNode as XElement;
+            return info.Element(name).Value;
+        }
+
+    }
+}
